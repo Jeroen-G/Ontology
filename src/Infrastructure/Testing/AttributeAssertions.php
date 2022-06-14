3f43917418c8c82e9273ee2eeb16378e9ed83fa6
Jeroen <jgroenendijk@enrise.com>
2022-06-14 20:13:41 +0200

    wip assertion helpers

---
+++ src/Infrastructure/Testing/AttributeAssertions.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JeroenG\Ontology\Infrastructure;
+
+trait AttributeAssertions
+{
+    public static function assertNoMethodNamesContaining(array $methods, string $needle): void
+    {
+        /** @var \ReflectionMethod $method */
+        foreach ($methods as $method) {
+            self::assertFalse(str_starts_with($method->name, $needle), "Found $needle in $method->name");
+        }
+    }
+}
